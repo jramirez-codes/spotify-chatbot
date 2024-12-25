471dea2bf8979f801c1201326e8a5e614b7dae21
Jordan Ramirez <jordan.s.ramirez@gmail.com>
2024-12-25 12:01:08 -0500

    Name on hover

--- src/components/imagesCard/image-card.tsx
+++ src/components/imagesCard/image-card.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { motion, AnimatePresence } from "motion/react";
+
 export function ImageCard(props: { obj: any }) {
   const [isHovering, setIsHovering] = React.useState(false);
   const [size] = React.useState(Math.floor(Math.random() * (35 - 10 + 1)) + 10);
@@ -7,20 +9,30 @@ export function ImageCard(props: { obj: any }) {
     <div style={{ margin: margin }} className="relative">
       <img
         onMouseEnter={() => {
-          setIsHovering(true);
-        }}
-        onMouseLeave={() => {
-          setIsHovering(false);
+          if (!isHovering) {
+            setIsHovering((_: boolean) => true);
+            setTimeout(() => {
+              setIsHovering((_: boolean) => false);
+            }, 1000);
+          }
         }}
         className="rounded object-contain drop-shadow-lg"
         style={{ minWidth: size + "vw", minHeight: size + "vw" }}
         src={props.obj.images[1].url}
       />
-      {isHovering && (
-        <div className="absolute top-0 left-0">
-          <h1>Test</h1>
-        </div>
-      )}
+      <AnimatePresence>
+        {isHovering && (
+          <>
+            <div className="absolute w-[100%]" style={{ top: Math.floor(size / 2) + "vh", left: 0 }}>
+              <motion.div key="box" exit={{ opacity: 0 }}>
+                <h1 className="font-mono font-bolder text-center text-7xl" style={{ paintOrder: "stroke fill" }}>
+                  {props.obj.name}
+                </h1>
+              </motion.div>
+            </div>
+          </>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
